ac6c3f874b2b47c8fbddb499e6a85f348aac5880
Quangg <65973279+quang-vn26@users.noreply.github.com>
2020-11-07 22:33:13 +0700

    'phut 16 error'

--- 2DFrogGame/main.cpp
+++ 2DFrogGame/main.cpp
@@ -1,11 +1,8 @@
 #include <math.h>//ap dung 1 so thuat toan ...
 
 #include <GL/glut.h>
-
 #include <iostream>
-
 #include <vector>
-
 //them mot so thu vien vao
 #include "../Library/Loadpng.h"
 #include "../Library/process_image.h"
@@ -16,12 +13,36 @@
 #define HEIGHT 384
 //fps dat 50 goi lai ham Display() moi 15s
 #define INTERVAL 15
+#define SCALE 3.0f
+#define RAD 57.295780
+#define START_X 0
+#define START_Y 0
+#define CELL_SIZE 8
+#define MAX_X 30
+#define MAX_Y 16
+#define CLOUD_COUNT 3
+#define PLATFORMER_COUNT 6
 
+Rect Rct_Background = {0,WIDTH,48,HEIGHT},Rct_Ground ={0,WIDTH,0,48};
+Image Img_Background,Img_Ground;
 //xu ly thay doi cua cua so, khi goi lai thi ve lai hoan toan
 void Display(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();//reset he toa do
+
+    Map_Texture($Img_Background);
+    Draw_Rect(&Rct_Background);
+    Map_Texture($Img_Ground);
+    Draw_Rect(&Rct_Ground);
+
 	glutSwapBuffers();//
+}
+
+void Init_Game(){
+    Load_Texture_Swap($Img_Background,"Image/Background.png");
+    Zoom_Image($Img_Background,SCALE);
+    Load_Texture_Swap($Img_Ground,"Image/Ground.png");
+    Zoom_Image($Img_Ground,SCALE);
 }
 
 void Init_GL() {
@@ -39,6 +60,8 @@ void Init_GL() {
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glEnable(GL_TEXTURE_2D);
+ 
+    Init_Game();
 }
 void Timer(int value){
     printf("Hello");
